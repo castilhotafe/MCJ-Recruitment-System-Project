ed237b5993a260928147d51eed594723f27499ff
MARCOS DE CASTILHO JUNIOR <20149083@tafe.wa.edu.au>
2025-06-18 00:13:22 +0800

    MSTest setting

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RecruitmentSystem recruitmentSystem = new RecruitmentSystem();
+        }
+    }
+}
